605ee5474c2da819923d6d4f4bb61f005cce6c9d
TaYaKi71751 <shantian.tw@gmail.com>
2022-01-19 17:48:35 +0900

    Create *

---
+++ README.md
@@ -0,0 +1,4 @@
+# Install 
+```
+sh -c "$(curl -LsSf https://raw.githubusercontent.com/raccl/setup/ubuntu/install.sh)"
+```
---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+from_repo(){
+    local PREFIX="https://raw.githubusercontent.com"
+    local ORG="$1"
+    local REPO="$2"
+    local BRANCH="$3"
+    local FPATH="$4"
+    local URL="${PREFIX}/${ORG}/${REPO}/${BRANCH}/${FPATH}"
+    sh -c "$(curl -LsSf ${URL})"
+}
+from_repo "raccl" "packages" "ubuntu" "install.sh"
+from_repo "raccl" "sudoers" "main" "tools/install.sh"
+from_repo "raccl" "agnoster-zsh-theme" "master" "tools/install.sh"
